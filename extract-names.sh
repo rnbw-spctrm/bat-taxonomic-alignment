0aed2ddc3379a7efdd52ce5ce58031493d60b012
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-02-21 17:59:54 -0600

    prepend name source to name list

--- extract-names.sh
+++ extract-names.sh
@@ -8,11 +8,10 @@ set -xe
 
 COMPLETE_TSV_HASH=$(preston track file://${PWD}/input/Complete.tsv | grep -oP "hash://sha256/[a-f0-9]{64}" | tail -n1)
 
-NAME_COLUMNS=$(preston cat ${COMPLETE_TSV_HASH} | head -n1 | tr '\t' '\n' | grep -oiP "^name_[a-zA-Z0-9]+")
+NAME_COLUMNS=$(preston cat ${COMPLETE_TSV_HASH} | head -n1 | tr '\t' '\n' | grep -oiP "^name_[a-zA-Z0-9]+" | tr '\n' ',')
 
 preston cat ${COMPLETE_TSV_HASH}\
- | mlr --tsvlite cut -f $(echo ${NAME_COLUMNS} | tr ' ' ',')\
- | tail -n+2\
- | tr '\t' '\n'\
+ | mlr --tsvlite cut -f $NAME_COLUMNS\
+ | mlr --itsvlite --ocsv reshape -i ${NAME_COLUMNS} -o id,scientificName\
  | sort\
  | uniq
