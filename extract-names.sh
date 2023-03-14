2152af605b82ef4c29d5210ec1f1a65f8709163b
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-03-14 11:02:24 -0500

    ensure to include [name MDD] column; https://github.com/jhpoelen/name-alignment-bats/issues/3

--- extract-names.sh
+++ extract-names.sh
@@ -8,10 +8,10 @@ set -xe
 
 COMPLETE_TSV_HASH=$(preston track file://${PWD}/input/Complete.tsv | grep -oE "hash://sha256/[a-f0-9]{64}" | tail -n1)
 
-NAME_COLUMNS=$(preston cat ${COMPLETE_TSV_HASH} | head -n1 | tr '\t' '\n' | grep -oiE "^name_[a-zA-Z0-9]+" | tr '\n' ',')
+NAME_COLUMN_PATTERN='^name[_\s][0-9a-zA-Z]+'
 
 preston cat ${COMPLETE_TSV_HASH}\
- | mlr --tsvlite cut -f $NAME_COLUMNS\
- | mlr --itsvlite --ocsv reshape -i ${NAME_COLUMNS} -o id,scientificName\
+ | mlr --tsvlite cut -r -f $NAME_COLUMN_PATTERN\
+ | mlr --itsvlite --ocsv reshape -r ${NAME_COLUMN_PATTERN} -o id,scientificName\
  | sort\
  | uniq
