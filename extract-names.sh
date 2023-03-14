b9d0a6a75b1acc73916fad11a1854bf5c8860c65
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-03-14 10:02:35 -0500

    use more standard grep option -E instead of -P

--- extract-names.sh
+++ extract-names.sh
@@ -6,9 +6,9 @@
 
 set -xe
 
-COMPLETE_TSV_HASH=$(preston track file://${PWD}/input/Complete.tsv | grep -oP "hash://sha256/[a-f0-9]{64}" | tail -n1)
+COMPLETE_TSV_HASH=$(preston track file://${PWD}/input/Complete.tsv | grep -oE "hash://sha256/[a-f0-9]{64}" | tail -n1)
 
-NAME_COLUMNS=$(preston cat ${COMPLETE_TSV_HASH} | head -n1 | tr '\t' '\n' | grep -oiP "^name_[a-zA-Z0-9]+" | tr '\n' ',')
+NAME_COLUMNS=$(preston cat ${COMPLETE_TSV_HASH} | head -n1 | tr '\t' '\n' | grep -oiE "^name_[a-zA-Z0-9]+" | tr '\n' ',')
 
 preston cat ${COMPLETE_TSV_HASH}\
  | mlr --tsvlite cut -f $NAME_COLUMNS\
