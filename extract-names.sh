3a5032d96ad416ea6781671684750fa5e0d84272
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-04-11 10:40:50 -0500

    add treatmentId columns as described in https://github.com/jhpoelen/name-alignment-bats/issues/4

--- extract-names.sh
+++ extract-names.sh
@@ -11,7 +11,11 @@ COMPLETE_TSV_HASH=$(preston track file://${PWD}/input/Complete.tsv | grep -oE "h
 NAME_COLUMN_PATTERN='^name[_\s][0-9a-zA-Z]+'
 
 preston cat ${COMPLETE_TSV_HASH}\
- | mlr --tsvlite cut -r -f $NAME_COLUMN_PATTERN\
- | mlr --itsvlite --ocsv reshape -r ${NAME_COLUMN_PATTERN} -o id,scientificName\
- | sort\
- | uniq
+ | nl\
+ | sed -E "s/^\s+//g"\
+ | sed -E "s/^1\t/treatmentId\t/g"\
+ | sed "s+^+https://linker.bio/line:${COMPLETE_TSV_HASH}!/L1,L+g"\
+ | sed "s+.*treatmentId+treatmentId+g"\
+ | mlr --tsvlite cut -r -f $NAME_COLUMN_PATTERN,treatmentId\
+ | mlr --itsvlite --ocsv reshape -r ${NAME_COLUMN_PATTERN} -o accordingTo,scientificName\
+ | mlr --tsvlite sort -f treatmentId
