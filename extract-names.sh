ae13f1377b6b58ac93c70e958c926203236dda7a
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-04-26 12:42:34 -0500

    fix data files

--- extract-names.sh
+++ extract-names.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 #
-# Extract names from Complete.tsv by Aja Sherman
+# Extract names from For_Pub_.tsv by Aja Sherman
 #
 # 2023-02-15 
 
@@ -19,4 +19,4 @@ preston cat ${COMPLETE_TSV_HASH}\
  | sed "s+.*treatmentId+treatmentId+g"\
  | mlr --tsvlite cut -r -f $NAME_COLUMN_PATTERN,treatmentId\
  | mlr --itsvlite --ocsv reshape -r ${NAME_COLUMN_PATTERN} -o accordingTo,scientificName\
- | mlr --tsvlite sort -f treatmentId
+ | mlr --csv sort -f treatmentId
